e068e11ec4b340136fc98969b76d429781db783e
LeeCheol <LeeCheol@DESKTOP-JMDBEM2>
2017-09-22 09:36:45 +0900

    2017-09-22,노드와 프로젝트 추가

---
+++ ConnectedIoT/src/com/OSS/ConnectedIoT/Node.java
@@ -0,0 +1,14 @@
+package com.OSS.ConnectedIoT;
+
+// 각각의 프로젝트에 추가하게 되는 노드들을 담는 클래스. IoT디바이스 명 등의 정보가 필요하다.
+public class Node {
+
+	
+	private String NodeName; // Node의 이름을 저장한다.
+	private String ProtocolType; // 통신하는 프로토콜의 타입을 지정한다.
+	private String NodeInfo; // 노드에 대한 설명
+	private String NodeLocation; // 노드 위치 등등.. 이건 String이 아니라 다른걸로 대체하는것이 좋을 수도 있겠다.
+	
+	
+	// 그외 어떤것들이 필요할지 계속 단계적으로 추가해 나가도록 한다.
+}
--- ConnectedIoT/src/com/OSS/ConnectedIoT/Project.java
+++ ConnectedIoT/src/com/OSS/ConnectedIoT/Project.java
@@ -1,11 +1,21 @@
 package com.OSS.ConnectedIoT;
 
+import java.util.ArrayList;
+
 // 프로젝트 단위로 관리되는 ConnectedIoT 특성상, 프로젝트를 가지고 있어야한다.
 public class Project { 
 
-	private String projectName = null;
-	private String projectInfo = null;
-	private String
+	private String projectName = null; // 프로젝트 이름
+	private String projectInfo = null; // 프로젝트 설명
+	private String createData = null; // 프로젝트 생성일
+	 
+	// 기타 등등 얼마든지 추가될 수 있다
 	
+	private ArrayList<Node> myNodes = null; // 노드들을 담을 ArrayList가 필요하다
+	
+	Project()
+	{
+		myNodes = new ArrayList<Node>(); // ArrayList를 초기화하고 생성하도록 한다.		
+	}
 	
 }
